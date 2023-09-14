68f8ed9fd2653370a82e23067a39ee256f24bb93
elvin <cabbarli.elvin75@gmail.com>
2023-09-14 18:58:11 +0400

    s

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,53 @@
 import React, { useState } from "react";
 import "./components.css";
 import logo from "../assets/logo.svg";
-import hammenu from "../assets/menu.png";
-import close from "../assets/close.png";
 
-const Navbar = () => {
-  const [menu, setMenu] = useState(false);
+const Header = () => {
+  const [closeMenu, setCloseMenu] = useState(true);
 
-  const toggleMenu = () => {
-    setMenu(!menu);
+  const handleCloseMenu = () => {
+    window.scrollTo(0, 0);
+    setCloseMenu(true);
   };
+
+  const handleMenuToggle = () => {
+    setCloseMenu((prev) => !prev);
+  };
+
   return (
-    <header>
-      <img src={logo} alt="" />
-      <nav className={menu ? "show-menu" : "hide-menu"}>
-        <a href="/">Startups</a>
-        <a href="/">Contact</a>
-        <button>Work with us!</button>
-      </nav>
-      {menu ? (
-        <img onClick={toggleMenu} className="ham-menu" src={close} alt="" />
-      ) : (
-        <img onClick={toggleMenu} className="ham-menu" src={hammenu} alt="" />
-      )}
-    </header>
+    <div className="header-wrapper">
+      <header className="container">
+        <div
+          className={
+            closeMenu
+              ? "header-container header-container-close"
+              : "header-container"
+          }
+        >
+          <input type="checkbox" name="" id="toggle" />
+          <a className="navbar-brand" href="/" onClick={handleCloseMenu}>
+            <img src={logo} alt="" />
+          </a>
+
+          <label
+            htmlFor="toggle"
+            className="navbar-toggler"
+            onClick={handleMenuToggle}
+          >
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </label>
+
+          <ul className="nav-list">
+            <li className="nav-item forum-nav">Startups</li>
+            <li className="nav-item">Contact</li>
+            <button>Work with us!</button>
+          </ul>
+        </div>
+      </header>
+    </div>
   );
 };
 
-export default Navbar;
+export default Header;
