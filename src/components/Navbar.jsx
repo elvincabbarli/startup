5e6074e2b7aed56b83947d1e92e8b7160df04874
elvin <cabbarli.elvin75@gmail.com>
2023-09-14 18:42:46 +0400

    14.09.2023

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import './components.css'
-import logo from '../assets/logo.svg'
+import React, { useState } from "react";
+import "./components.css";
+import logo from "../assets/logo.svg";
+import hammenu from "../assets/menu.png";
+import close from "../assets/close.png";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setMenu(!menu);
+  };
   return (
     <header>
-    <img src={logo} alt="" />
-    <nav>
-      <a href="/">Startups</a>
-      <a href="/">Contact</a>
-      <button>Work with us!</button>
-    </nav>
-  </header>
-  )
-}
+      <img src={logo} alt="" />
+      <nav className={menu ? "show-menu" : "hide-menu"}>
+        <a href="/">Startups</a>
+        <a href="/">Contact</a>
+        <button>Work with us!</button>
+      </nav>
+      {menu ? (
+        <img onClick={toggleMenu} className="ham-menu" src={close} alt="" />
+      ) : (
+        <img onClick={toggleMenu} className="ham-menu" src={hammenu} alt="" />
+      )}
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
